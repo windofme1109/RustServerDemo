341992aae56a67f175730497896a6633b7d032fd
windofme1109 <wuzengyan1314@163.com>
2024-07-22 20:22:56 +0800

    基本的 http 服务器 - http 请求体 - 1

---
+++ s2/http/src/httprequest.rs
@@ -0,0 +1,156 @@
+use std::{collections::HashMap};
+
+
+/**
+ * 定义 Http Request 结构
+ * 
+ */
+
+//  定义请求方法
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    Get,
+    Post,
+    Uninitialized
+}
+
+/**
+ * 将传进来的字符串切片转换为 Method 枚举
+ */
+impl From<&str> for Method {
+    fn from(s: &str) -> Self {
+        match s {
+           Get => Method::Get,
+           Post => Method::Post,
+            _ => Method::Uninitialized
+        }
+    }
+}
+
+// 定义 http 协议版本
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Self {
+        match s {
+            "http/1.1" => Version::V1_1,
+            "http/2" => Version::V2_0,
+            _ => Version::Uninitialized
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Resource {
+    Path(String)
+}
+
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub headers: HashMap<String, String>,
+    pub resource: Resource,
+    pub msg_body: String,
+}
+
+impl From<String> for HttpRequest {
+    fn from(req: String) -> Self {
+        // 初始化请求方法、协议版本、请求资源（路径）
+        let mut parsed_method = Method::Uninitialized;
+        let mut parsed_version = Version::Uninitialized;
+        let mut parsed_resource = Resource::Path("".to_string());
+
+        let mut parsed_headers: HashMap<String, String> = HashMap::new();
+
+        let mut parsed_msg_body = "".to_string();
+
+        for line in req.lines() {
+            
+            if line.to_lowercase().contains("http") {
+                // 解析首行
+                let (method, version, resource) = process_req_line(line);
+                parsed_method = method;
+                parsed_version = version;
+                parsed_resource = resource;
+            } else if line.contains(":") {
+                // 解析 header
+                let (key, value) = process_header_line(line);
+                // 将获取到的请求头键值对插入 headers 这个 hashMap 中
+                parsed_headers.insert(key, value);
+                
+            } else {
+                parsed_msg_body = line.to_string();
+            }
+        }
+
+
+        HttpRequest {
+            method: parsed_method,
+            version: parsed_version,
+            headers: parsed_headers,
+            resource: parsed_resource,
+            msg_body: parsed_msg_body,
+        }
+
+    }
+}
+
+// 对 http 请求的首行进行解析
+fn process_req_line(s: &str) -> (Method, Version, Resource) {
+    // http 请求的首行格式：请求方法 请求路径 协议版本\r\n
+    let mut content = s.split_ascii_whitespace();
+    let method = content.next().unwrap();
+    let path = content.next().unwrap();
+    let version = content.next().unwrap();
+
+    // (
+    //     Method::from(method),
+    //     Version::from(version),
+    //     Resource::Path(path.to_string()),
+
+    // )
+
+    (
+        method.into(),
+        version.into(),
+        Resource::Path(path.to_string()),
+    )
+}
+
+
+fn process_header_line(s: &str) -> (String, String) {
+    let mut header = s.split("");
+    let mut key = "".to_string();
+    let mut val = "".to_string();
+
+    if let Some(k) = header.next() {
+        key = k.to_string();
+    }
+
+    if let Some(v) = header.next() {
+        val = v.to_string();
+    }
+   
+    (
+        key,
+        val
+    )
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+
+        assert_eq!(m, Method::Get);
+    }
+}
